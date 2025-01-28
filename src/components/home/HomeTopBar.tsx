50a3549d0c3633c76f34c4f71ead5ec2306aa1f1
Adebayo Muis <charmingdc002@gmail.com>
2025-01-28 21:38:57 +0100

    Styled navbar

--- src/components/home/HomeTopBar.tsx
+++ src/components/home/HomeTopBar.tsx
@@ -1,12 +1,24 @@
+import { HiMenuAlt1 } from "react-icons/hi"
+import { LuSearch } from "react-icons/lu";
+
+import './index.css'
+
 const HomeTopBar = () => {
   return (
     <nav>
-     <ul className='home-topbar'>
-       <li></li>
+     <ul className='flex-between home-topbar'>
+       <li className='flex-center'>
+        <HiMenuAlt1 
+          className='home-topbar-icon'
+          style={{width: '2.8rem', height: '2.8rem'}}/>
+       </li>
        
-       <li>
-         <span></span>
-         <div></div>
+       <li className='flex-center'>
+        <LuSearch className='home-topbar-icon' />
+        
+        <div className='flex-center'>
+          <img src='/illustrations/default-dp.webp' alt='User avatar' />
+        </div>
        </li>
      </ul>
     </nav>
